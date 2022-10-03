550b0072ded2431430d26f32a4752dafcf58ea10
mastershashank <beingshashank08@gmail.com>
2022-10-04 00:46:55 +0530

    Initial Commit

---
+++ localVariable.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract LocalVar {
+function getLocal() public pure returns(uint)
+{
+       uint rollNumber = 10;
+        return rollNumber;
+    
+}
+}
