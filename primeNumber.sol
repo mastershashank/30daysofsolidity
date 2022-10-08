6c10af683f9518cb8d0e49ff88e7578a39b86442
mastershashank <beingshashank08@gmail.com>
2022-10-08 20:10:55 +0530

    Initial Commit

---
+++ primeNumber.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day3 {
+    function prime(uint256 n) public pure returns(uint256) {
+
+ if (n <= 1) {
+            return 0; //not prime
+}
+    for(uint256 i = 2; i < n; i++) {
+    if( n % i == 0) { 
+     return 0;
+    }
+
+    else {
+    return 1;
+    }
+   
+    }}   
+}
