a50338839e19e9760ce2931e5bff7d24abb66d02
mastershashank <beingshashank08@gmail.com>
2022-10-10 07:28:06 +0530

    Initial Commit

---
+++ contrpract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract demo {
+    uint public num;
+    constructor(uint c){
+      num = c;
+    }
+
+    function change(uint a) public {
+        num = a;
+    }
+
+
+}
